d407affb8cc30255a6d1209f65afd4aa248f23f3
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-01 14:53:10 +0900

    body リクエストの追加

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(): string {
+  create(@Body() createCatDto: CreateCatDto): string {
+    console.log(createCatDto);
     return 'This action adds a new cat';
   }
 
