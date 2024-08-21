7192ee5f3e4162e5f3ee4bac1d26b938b9cb621f
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-08-21 15:54:47 +0900

    userテーブルの取得

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import * as mysql2 from 'mysql2';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    const connection = mysql2.createConnection({
+      host: 'localhost',
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: 'my_app_database',
+    });
+
+    connection.connect((err) => {
+      if (err) {
+        console.log('error connecting: ' + err.stack);
+        return;
+      }
+      console.log('success');
+    });
+
+    connection.query('SELECT * FROM users', (error, results) => {
+      console.log(results);
+    });
+
     return 'Hello World!';
   }
 }
