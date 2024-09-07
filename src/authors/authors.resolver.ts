2be57e9c0ca36aff8a8dfe6421c5affd67fc805b
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-09-07 16:14:35 +0900

    AuthorsResolverのメソッド名変更

--- src/authors/authors.resolver.ts
+++ src/authors/authors.resolver.ts
@@ -9,6 +9,7 @@ import {
 import { Author } from './models/author.model.js';
 import { AuthorsService } from './authors.service.js';
 import { PostsService } from '../post/posts.service.js';
+import { Post } from '../post/models/post.model.js';
 
 @Resolver(() => Author)
 export class AuthorsResolver {
@@ -17,13 +18,13 @@ export class AuthorsResolver {
     private postsService: PostsService,
   ) {}
 
-  @Query(() => Author)
-  author(@Args('id', { type: () => Int }) id: number) {
+  @Query(() => Author, { name: 'author' })
+  getAuthor(@Args('id', { type: () => Int }) id: number) {
     return this.authorsService.findOneById(id);
   }
 
-  @ResolveField()
-  posts(@Parent() author: Author) {
+  @ResolveField('posts', () => [Post])
+  getPosts(@Parent() author: Author) {
     const { id } = author;
     return this.postsService.findAll({ authorId: id });
   }
