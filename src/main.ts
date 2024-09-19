6295163659b2a420bbbd635c5b2ed661f01b42c3
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-09-19 21:15:03 +0900

    basicディレクトリ作成と整理

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ApolloAppModule } from './apollo/apollo-app.module.js';
-import { AppModule } from './app.module.js';
+import { AppModule } from './basic/app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types.js';
-import { AppModule } from '../src/app.module.js';
+import { AppModule } from '../src/basic/app.module.js';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
